6f4b2d17ceb5a3149aa57fb126b9a359cf782932
Milo Silva <milo.silva234@gmail.com>
2021-01-18 10:20:59 +0100

    add webSocket listeners to handle events: - user/join client sends user token, server adds user details to socket - user/likedMovie & user/dislikedMovie A new association is added to the database between the movie and user.

--- socket/index.js
+++ socket/index.js
@@ -1,24 +1,68 @@
+const { userFromToken } = require("../auth/jwt");
+const UserMovie = require("../models").userMovie;
+
+// socket handler
 module.exports = function socketHandler(socket) {
-  console.log(`new connection id: ${socket.id}`);
+  console.log(`new connection: ${socket.id}`);
 
-  socket.on("join", (stuff) => {
-    // what user thing should I pass?
-    socket.user = stuff;
-    // add user to the room of other people in same group??
+  socket.on("user/join", async (userToken) => {
+    try {
+      const user = await userFromToken(userToken);
+      socket.user = user;
+      const { name, partyId } = user;
+      // what if userMovie had a partyId ?? then could just query the partyId of the user to get the movies to show.
 
-    //
+      // each party has their own room;
+      // whenever a user in a party likes/dislikes movie,
+      // movie is added to other users staging lists...?
+      // event is emited to all other members of party?
+      // other user clients then get that movie and add it to their staging list?
+      // or, added when they fetch a new list from /staginglist...??
+      // OOOR, just socket.emit an event to the party room with the movie??
+      socket.join(partyId);
+      console.log(`[socket]user connected: ${name} joined party: ${partyId}`);
+    } catch (e) {
+      console.log(`[socket] error getting user from DB`);
+    }
   });
-  socket.on("movie liked", (movieId) => {
+
+  socket.on("user/likedMovie", async (movie) => {
     // Add movie to userMovies (liked: true)
     // Add movie to staging list of other users (or just do that in the stagingList route...?)
     // Where to check for a movie match?
+    if (!socket.user) return; // need to have a user.
+    const { name, id: userId, partyId } = socket.user;
+    const { id: movieId, title } = movie;
+
+    try {
+      // create new association
+      const newUserMovie = await UserMovie.create({ userId, movieId, liked: true });
+      console.log(
+        `[socket][${partyId ? "group: " + partyId : ""}]movie ${title} liked by: ${name} `
+      );
+      socket.to(partyId).emit("party/movieLiked", movie);
+    } catch (error) {
+      console.log(`[socket]:${error}`);
+    }
   });
-  socket.on("movie disliked", (movieId) => {
+
+  socket.on("user/dislikedMovie", async (movie) => {
     // Add movie to userMovies (liked: false)
     // Add movie to staging list of other users (or just do that in the stagingList route...?)
     // Where to check for a movie match?
     /* on match:  
-
     - socket.send() */
+    if (!socket.user) return; // need to have a user.
+    const { name, id: userId, partyId } = socket.user;
+    const { id: movieId, title } = movie;
+    try {
+      const newUserMovie = await UserMovie.create({ userId, movieId, liked: true });
+      socket.to(partyId).emit("party/movieLiked", movie);
+      console.log(
+        `[socket][${partyId ? "group: " + partyId : ""}]movie ${movieId} disliked by: ${name}`
+      );
+    } catch (error) {
+      console.log(`[socket]:${error}`);
+    }
   });
 };
