7ba4d5256aed628f2c1737c044ffa016da594db2
Milo Silva <milo.silva234@gmail.com>
2021-01-19 12:06:09 +0100

    Test & update the staging algorithm

--- routers/stagingList.js
+++ routers/stagingList.js
@@ -6,19 +6,12 @@ const User = Models.user;
 const UserMovie = Models.userMovie;
 const { Op } = require("sequelize");
 
-// GET /stagedList - Returns a staged list of 5 movies to show to the user.
-// user must not have previously interacted with the movies
-//
+// GET /stagedList - Returns a staged list of 10 movies to show to the user.
+// user must not have previously interacted with the movies.
 router.get("/", auth, async (req, res, next) => {
-  // use auth later on
-  // get movies from backend
-  // where
-  // send movies to FrontEnd
-  const { page } = req.query;
-  console.log(`fetching page: ${page}`);
   const { id: userId, partyId } = req.user;
   try {
-    // get group movies
+    // get all group movies
     const groupMovies = await User.findAll({
       attributes: ["id"],
       include: [
@@ -27,42 +20,51 @@ router.get("/", auth, async (req, res, next) => {
       where: { partyId },
     });
 
-    // get unique Movies That User Hasnt interacted with But A User In Group Has liked
+    // get unique Movies That User Hasnt interacted with But Another User In Group Has liked
     const userMovieIdList = groupMovies
       .find((user) => user.id === userId)
       .dataValues.movies.map((movie) => movie.id);
     console.log("userIdList length:", userMovieIdList.length);
+    // filter for lists of all other users in the group
     const groupLists = groupMovies
       .filter((user) => user.id !== userId)
-      .map((user) => user.dataValues.movies); // gets the lists of all users in the group
+      .map((user) => user.dataValues.movies);
 
-    // combine the lists
+    // combine lists into the same level of array
     const flattenedList = groupLists.flat(1);
 
+    // filter for movies that have been liked
     const GroupLikedList = flattenedList
       .filter((movie) => movie.userMovies.dataValues.liked)
       .map((movie) => movie.dataValues);
 
+    // filter for movies the current user hasn't previously interacted with
     const finalList = GroupLikedList.filter((movie) => !userMovieIdList.includes(movie.id));
-    console.log(finalList.length);
+    console.log("movies liked by group:", finalList.length);
+
+    // ideally, should never have more than half the movies from other people's likes.
 
     if (finalList.length < 10) {
+      // add new movies to the list to increase the length to 10.
       const amount = 10 - finalList.length;
       const extraItems = await Movie.findAll({
         limit: amount,
         where: { id: { [Op.notIn]: userMovieIdList } },
       });
       console.log("extraItems", extraItems.length);
-      console.log("finalList", finalList.length);
       finalList.push(...extraItems.map((item) => item.dataValues));
     }
-    // console.log(finalList.length);
-    res.send(shuffle(finalList));
+    console.log("final list length:", finalList.length); // should be 10
+    const shuffled = shuffle(finalList);
+    // console.log("first movie:", shuffled[0].title);
+    res.send(shuffled);
   } catch (error) {
     console.log(`[stagingList]: ${error}`);
     next(error);
   }
 });
+
+// Use Fisher Yates algorithm to randomise staging order
 function shuffle(arr) {
   const array = arr;
   for (let i = array.length - 1; i > 0; i--) {
@@ -72,6 +74,7 @@ function shuffle(arr) {
   return array;
 }
 
+// static staging list implementation (backup)
 /* router.get("/", auth, async (req, res, next) => {
   const { page } = req.query;
   console.log(`fetching page: ${page}`);
