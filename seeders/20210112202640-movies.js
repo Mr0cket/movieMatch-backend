f7b1498bc7cb2dbf18d60e4511d2b2f6fb67684b
Milo Silva <milo.silva234@gmail.com>
2021-01-19 10:43:23 +0100

    validate relevant fields exist before posting toDB

--- seeders/20210112202640-movies.js
+++ seeders/20210112202640-movies.js
@@ -3,12 +3,11 @@ const axios = require("axios");
 const pages = require("../config/constants").movieSeedPages;
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const endpoint =
-      "https://api.themoviedb.org/3/discover/movie?api_key=eb066629e9e5aca99797f3955400c4bd&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1";
+    const endpoint = `https://api.themoviedb.org/3/discover/movie?api_key=eb066629e9e5aca99797f3955400c4bd&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=`;
     try {
       let combinedPagesList = [];
-      for (let i = 0; i < pages; i++) {
-        const res = await axios.get(endpoint);
+      for (let i = 1; i <= pages; i++) {
+        const res = await axios.get(endpoint + i);
         // need to properly parse data to make it fit table columns
         const originalList = res.data.results;
         const parsedList = originalList.map((movie) => {
@@ -21,18 +20,29 @@ module.exports = {
             vote_average: rating,
           } = movie;
           const posterUrl = "https://image.tmdb.org/t/p/w500" + poster_path;
-          return {
-            movieId,
-            title,
-            overview,
-            posterUrl,
-            releaseDate,
-            rating,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          };
+          if (releaseDate)
+            return {
+              movieId,
+              title,
+              overview,
+              posterUrl,
+              releaseDate,
+              rating,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            };
+          else
+            return {
+              movieId,
+              title,
+              overview,
+              posterUrl,
+              rating,
+              createdAt: new Date(),
+              updatedAt: new Date(),
+            };
         });
-        combinedPagesList = [...parsedList, ...combinedPagesList];
+        combinedPagesList = [...combinedPagesList, ...parsedList];
       }
       await queryInterface.bulkInsert("movies", combinedPagesList);
     } catch (e) {
