50d59f4255d20b7d2ebec7775222a12a9285a2e4
Milo Silva <milo.silva234@gmail.com>
2021-01-19 17:13:38 +0100

    configure socket.io for production deployment

--- index.js
+++ index.js
@@ -2,20 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const corsInstance = cors();
-const httpServer = require("http").createServer(app);
+// const httpServer = require("http").createServer(app);
 const logMiddleware = require("morgan");
 const jsonParser = express.json();
-
-// initialise socket.io & configure CORS
-const io = require("socket.io")(httpServer, {
-  cors: {
-    origin: "*",
-  },
-});
-
-// handle web-socket connections
-const socketHandler = require("./socket");
-io.on("connection", socketHandler);
 
 // Middleware
 app.use(corsInstance);
@@ -37,9 +26,20 @@ app.use("/movies", moviesRouter);
 // Execute server process
 const internalIp = require("internal-ip").v4.sync();
 const port = process.env.PORT || 4000;
-httpServer.listen(port, () =>
+const httpServer = app.listen(port, () =>
   console.log(`listening on:
 local:  localhost:${port}
 lan:    ${internalIp}:${port}
 `)
 );
+
+// initialise socket.io & configure CORS
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// handle web-socket connections
+const socketHandler = require("./socket");
+io.on("connection", socketHandler);
